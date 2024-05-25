b8575f2f462438c36a61fa32a5ae3d2bdffbffbb
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2024-05-25 15:25:37 -0700

    Add missing Buffer_String.(c|h) and parsing_Test.c

---
+++ primitives/Buffer_String.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "./Buffer_String.h"
+
+
+struct Buffer_String* Buffer_String_init(int capacity)
+{
+	if (capacity < 0)
+	{
+		capacity = 0;
+	}
+
+	struct Buffer_String* retval = (struct Buffer_String*)malloc(sizeof(struct Buffer_String) + capacity * sizeof(struct String));
+
+	if (retval != NULL)
+	{
+		retval->capacity = capacity;
+		retval->length = 0;
+	}
+
+	return retval;
+}
+
+struct Buffer_String* Buffer_String_filter(struct Buffer_String* self, bool (*filter)(struct String))
+{
+	int capacity = self->capacity;
+	struct String* buffer = self->buffer;
+	struct Buffer_String* retval = Buffer_String_init(capacity);
+
+	for (int idx = 0; idx < capacity; idx++)
+	{
+		struct String item = buffer[idx];
+
+		if (filter(item))
+		{
+			Buffer_String_push(retval, item);
+		}
+	}
+
+	return retval;
+}
+
+bool Buffer_String_push(struct Buffer_String* self, struct String item)
+{
+	if (self == NULL || self->capacity <= self->length)
+		return false;
+	self->buffer[self->length++] = item;
+	return true;
+}
+
+bool Buffer_String_pop(struct Buffer_String* self)
+{
+	if (self == NULL || self->length <= 0)
+	{
+		return false;
+	}
+
+	self->length--;
+
+	return true;
+}
+
+bool Buffer_String_swap(struct Buffer_String* self, int left_idx, int right_idx)
+{
+	if (self == NULL)
+	{
+		return false;
+	}
+
+	int buffer_length = self->length;
+	struct String* buffer = self->buffer;
+
+	if (!(0 <= left_idx && left_idx < buffer_length && 0 <= right_idx && right_idx < buffer_length))
+	{
+		return false;
+	}
+
+	struct String temp = buffer[left_idx];
+	buffer[left_idx] = buffer[right_idx];
+	buffer[right_idx] = temp;
+
+	return true;
+}
---
+++ tests/parsing_Test.c
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <string.h>
+
+#include "../parsing.h"
+#include "../primitives/Buffer_Parametric_Binding.h"
+#include "../primitives/String.h"
+
+struct Arena zero;
+
+struct Parametric_Binding binding(char c, char* type_name, char* instance_name)
+{
+	struct String _type_name = String_from_cstring(type_name);
+	struct String _instance_name = String_from_cstring(instance_name);
+	struct Parametric_Binding binding = {
+		.parametric = c,
+		.type_name = _type_name,
+		.type_instance = _instance_name
+	};
+
+	return binding;
+}
+
+void assert_type_name(struct Buffer_Parametric_Binding const* bindings,
+		char* template, char* output)
+{
+	struct Arena arena = zero;
+	struct String _template = String_from_cstring(template);
+	struct String parsed = match_type_name(bindings, &arena, _template, 0);
+
+	if (output != NULL)
+	{
+		assert((int)strlen(output) == parsed.length);
+		assert(strncmp(parsed.str, output, parsed.length) == 0);
+	}
+	else
+	{
+		assert(parsed.str == NULL);
+	}
+}
+
+void assert_instance_name(struct Buffer_Parametric_Binding const* bindings,
+		char* template, int offset, char* output)
+{
+	struct String _template = String_from_cstring(template);
+	struct String parsed = match_instance_name(bindings, _template, offset);
+
+	if (output != NULL)
+	{
+		assert((int)strlen(output) == parsed.length);
+		assert(strncmp(parsed.str, output, parsed.length) == 0);
+	}
+	else
+	{
+		assert(parsed.str == NULL);
+	}
+}
+
+int main()
+{
+	zero = Arena_init(100);
+	struct Buffer_Parametric_Binding* bindings =
+			Buffer_Parametric_Binding_init(4);
+
+	Buffer_Parametric_Binding_push(bindings, binding('A', "WeirdFishes",
+			"struct WeirdFishes"));
+	Buffer_Parametric_Binding_push(bindings, binding('B', "Idioteque",
+			"struct Idioteque"));
+	Buffer_Parametric_Binding_push(bindings, binding('C',
+			"House_of_Cards", "struct House_of_Cards"));
+	Buffer_Parametric_Binding_push(bindings, binding('T', "Int",
+			"int"));
+
+	assert_type_name(bindings, "<A,B>", "WeirdFishes_Idioteque");
+	assert_type_name(bindings, "<B,B,B>", "Idioteque_Idioteque_Idioteque");
+	assert_type_name(bindings, "<A    B>",  "WeirdFishesIdioteque");
+	assert_type_name(bindings, "<B\nB\n\nB>", "IdiotequeIdiotequeIdioteque");
+
+	assert_type_name(bindings, "<a,B>", NULL);
+	assert_type_name(bindings, "<B,B;B>", NULL);
+	assert_type_name(bindings, "<Ab>", NULL);
+	assert_type_name(bindings, "<B'BB>", NULL);
+
+	assert_instance_name(bindings, "T", 0, "int");
+	assert_instance_name(bindings, "(T param", 1, "int");
+	assert_instance_name(bindings, "\tT* ptr", 1, "int");
+	assert_instance_name(bindings, "\nC", 1, "struct House_of_Cards");
+}
